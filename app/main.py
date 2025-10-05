d9a0aa8e2857fb7da8ea20afb0f0b31d4e4f0416
Dmytro Shevchuk <shevchukkdmytro@gmail.com>
2025-10-05 14:54:32 +0300

    remake main with maps and match

--- app/main.py
+++ app/main.py
@@ -4,22 +4,35 @@ from app.book_service import BookServiceConsole, BookServiceReverse
 
 
 def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
-    for cmd, method_type in commands:
-        if method_type == "console":
-            book_service = BookServiceConsole()
-        elif method_type == "reverse":
-            book_service = BookServiceReverse()
-        elif method_type == "xml":
-            serializer = BookXMLSerializer()
-        elif method_type == "json":
-            serializer = BookJSONSerializer()
+    service_map = {
+        "console": BookServiceConsole,
+        "reverse": BookServiceReverse,
+    }
+    serializer_map = {
+        "json": BookJSONSerializer,
+        "xml": BookXMLSerializer,
+    }
 
-        if cmd == "display":
-            book_service.display(book)
-        elif cmd == "print":
-            book_service.print_book(book)
-        elif cmd == "serialize":
-            return serializer.serialize(book)
+    for cmd, method_type in commands:
+        if method_type in ["console", "reverse"]:
+            book_service = service_map.get(method_type)()
+
+            match cmd:
+                case "display":
+                    book_service.display(book)
+                case "print":
+                    book_service.print_book(book)
+                case _:
+                    raise ValueError(f"Unknown command {cmd}")
+
+        elif method_type in ["json", "xml"]:
+            serializer = serializer_map.get(method_type)()
+
+            match cmd:
+                case "serialize":
+                    return serializer.serialize(book)
+                case _:
+                    raise ValueError(f"Unknown command {cmd}")
 
 
 if __name__ == "__main__":
